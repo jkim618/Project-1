23260174962b4e4d232d1b9a2420db1d13402e01
jkim618 <48166268+jkim618@users.noreply.github.com>
2019-05-22 17:45:33 -0400

    Update restaurants.js

--- src/restaurants.js
+++ src/restaurants.js
@@ -2,6 +2,10 @@ import React , {component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import App from "./App.js"
+import Map from './map.js';
+import MapView from './marker.js'
+import './styles.css'
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
@@ -10,28 +14,36 @@ class Restaurants extends React.Component{
     super(props);
     this.state = {
       rest_names: [],
-      rest_names2: []
+      lats: [],
+      lngs: []
     }
   }
 
   componentDidMount(){
-    let url = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.028507,-78.510606&radius=3300&type=restaurant&opennow=True&key=' + API_KEY;
+    let url = 'https://cors-anywhere-hclaunch.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.028507,-78.510606&radius=3300&type=restaurant&opennow=True&key=' + API_KEY;
     axios.get(url).then(response=>{
     console.log(response);
     const data2 = response.data.results
-    this.setState({rest_names:data2})
+    const data3 = response.data.results.map((x)=>x.geometry).map((y)=>y.location).map((z)=>z.lat)
+    const data4 = response.data.results.map((x)=>x.geometry).map((y)=>y.location).map((z)=>z.lng)
+    this.setState({rest_names:data2, 
+                lats:data3,
+                lngs:data4})
   });
 }
 render(){
   return (
-    <div>
+    <div class="list">
+        <h1> Best Restaurants </h1>
       <ul>
           {this.state.rest_names.map(x => (
-            <li key={x.id}> Name: {x.name} | Rating: {x.rating} | Price Level: {x.price_level}</li>
+            <li key={x.id}> 
+            <li id="restname">{x.name}</li>  
+            <li id="rating">Rating: {x.rating}</li>
+            <li id="price">Price Level: {x.price_level}</li><br></br></li>
           ))}
+          {/* <MapView lats={this.state.lats}/> */}
         </ul>
-        {this.state.rest_names.map((x)=>x.geometry).map((y)=>y.location).map((z)=>z.lat)}
-        {this.state.rest_names.map((x)=>x.geometry).map((y)=>y.location).map((z)=>z.lng)}
     </div>
   );
 }
